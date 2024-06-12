78b116acd8b371f5b15182d59284a9c6f5a58eac
Hoang Viet Anh <vietanh092004@gmail.com>
2024-06-12 21:21:25 +0700

    Fix: natas3.md

--- natas3/solve.py
+++ natas3/solve.py
@@ -3,12 +3,12 @@ import requests
 url = 'http://natas3.natas.labs.overthewire.org'
 
 s = requests.Session()
-s.auth = ('natas3', 'G6ctbMJ5Nb4cbFwhpMPSvxGHhQ7I6W8Q')
+s.auth = ('natas3', '3gqisGdR0pjm6tpkDKdIWO2hSvchLeYH')
 url_check = url + "/robots.txt"
 r = s.get(url_check)
 print(r.text)
 print('------------------')
-url += '/s3cr3t/'
+url += '/s3cr3t/users.txt'
 r = s.get(url)
 print(r.text)
 
